80d9f59263669f233364d8d8c01eb0cc9a76ba07
TofiOrTofa <mavder76@gmail.com>
2026-01-22 00:37:00 +0300

    feat: добавил решение примеров с несколькими знаками

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ class sum_calculate(abstract_calculator, compilate_string):#класс для с
     def compilate(self, num_first, num_second):
         return super().compilate_sif(num_first, num_second)
     def score(self, nums):
-        return nums[0]+nums[1]
+        return str(nums[0]+nums[1])
 class subtraction_calculate(abstract_calculator, compilate_string):#класс для вычитания чисел
     def compilate(self, num_first, num_second):           
         return super().compilate_sif(num_first, num_second)
     def score(self, nums):
-        return nums[0]-nums[1]
+        return str(nums[0]-nums[1])
 
 
 
@@ -67,40 +67,47 @@ class score_expression:
         self.numbers : set
         self.expression : str
         self.expression_list : list
-        self.Texpression_start : int
-        self.Texpression_end : int
-        self.Texpression_list : list           
+        self.SaveExpression : tuple
+        self.TimeExpression_list : list
+        self.TimeExpression_start : int
+        self.TimeExpression_end : int          
         self.mathematical_sd : dict[str:list(class_sing_calculator, int)] = mathematical_sd
-        self.two_expression : tuple(str, ...)
+        self.TwoExpression : tuple(str, ...)
+        self.TwoExpression_start : int
         self.backets : bool#наличие скобок в примере
+        self.answer : int or float
 
         #mathematical_sings:dict[str:list(str, int)]={}#знак:[класс, значимость]
         self.numbers={'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
         self.expression=input("Введите выражение: ").strip()
         self.expression_list=list()
-        self.Texpression_list=list()
+        self.TimeExpression_list=list()
 
         self.compilate_list()
         if "(" in self.expression_list:
             self.backets=True
+            self.SaveExpression=tuple(self.expression_list)
             self.find_and_compilate_backets_expression()
         else:
             self.backets=False
-            self.Texpression_list=self.expression_list
-            self.Texpression_start=0
-            self.Texpression_end=-1
-        self.find_two_expression()
-        self.score_two_expression()
+            self.TimeExpression_list=self.expression_list
+            self.TimeExpression_start=0
+            self.TimeExpression_end=-1
+        while len(self.TimeExpression_list)!=1:
+            self.find_TwoExpression()
+            self.score_TwoExpression()
+            self.rm_TwoExpression()
+        print(self.expression_list)
 
 
     def find_and_compilate_backets_expression(self):
         for i in range(len(self.expression_list)):
-            if self.expression_list[i]=="(": self.Texpression_start=i
+            if self.expression_list[i]=="(": self.TimeExpression_start=i
             elif self.expression_list[i]==")":
-                self.Texpression_end=i
+                self.TimeExpression_end=i
                 #ответ поставится на место первой скобки, а остольное удалится, можно будет сделать ещё, что бы проверялось есть ли после скобки и до скобки цифра, и если есть, то пишется умножение
-                self.Texpression_list=self.expression_list[self.Texpression_start+1:self.Texpression_end]
-                #print(self.Texpression_list)
+                self.TimeExpression_list=self.expression_list[self.TimeExpression_start+1:self.TimeExpression_end]
+                #print(self.TimeExpression_list)
 
                 #нужно дописать, что бы функция вызывала функцию с решением,
                 #которая будет решать пример в скобках как обычный и возвращать ответ ввиде числа
@@ -113,17 +120,22 @@ class score_expression:
                   #в первом он должен вызвать сам себя сделав что-то типо рекурсии.
                   #а во втором он должен вернуть какое-нибудь число, или строку, обозначающую, что его надо вызвать повторно
 
-    def find_two_expression(self):
+    def find_TwoExpression(self):
         a=1
-        for i in range(len(self.Texpression_list)):
-            if self.Texpression_list[i] in self.mathematical_sd and self.mathematical_sd[self.Texpression_list[i]][1]==a:
-                self.two_expression:tuple(str, ...)=(self.Texpression_list[i-1:i+2])
+        for i in range(len(self.TimeExpression_list)):
+            if self.TimeExpression_list[i] in self.mathematical_sd and self.mathematical_sd[self.TimeExpression_list[i]][1]==a:
+                self.TwoExpression:tuple(str, ...)=(self.TimeExpression_list[i-1:i+2])
+                self.TwoExpression_start=i-1
                 return
-    def score_two_expression(self):
-        #print(self.two_expression[0], self.two_expression[2])
-        #print(self.mathematical_sd[self.two_expression[1]][0].compilate(self.two_expression[0], self.two_expression[2]))
-        print(self.mathematical_sd[self.two_expression[1]][0].score(self.mathematical_sd[self.two_expression[1]][0].compilate(self.two_expression[0], self.two_expression[2])))
-    def rm_two_expression(self): ...# ---<недоделано>---
+    def score_TwoExpression(self):
+        #print(self.TwoExpression[0], self.TwoExpression[2])
+        #print(self.mathematical_sd[self.TwoExpression[1]][0].compilate(self.TwoExpression[0], self.TwoExpression[2]))
+        self.answer=self.mathematical_sd[self.TwoExpression[1]][0].score(self.mathematical_sd[self.TwoExpression[1]][0].compilate(self.TwoExpression[0], self.TwoExpression[2]))
+        print(self.answer)
+    def rm_TwoExpression(self):
+        del self.TimeExpression_list[self.TwoExpression_start+1:self.TwoExpression_start+3]
+        self.TimeExpression_list[self.TwoExpression_start]=self.answer
+        print(self.TimeExpression_list)
     def compilate_list(self):
         a=0
         for i in self.expression:
@@ -133,9 +145,9 @@ class score_expression:
             elif i=="(" or i==")": self.expression_list.append(i); a=3
             elif i=="." or i==",": self.expression_list[-1]+=i; a=4
             else:
-                if not a: self.expression_list[-1]+=i
+                if not a or a==4: self.expression_list[-1]+=i
                 else: self.expression_list.append(i); a=0
-#print(self.expression_list)
+
 
 
 
