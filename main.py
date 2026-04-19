21a6788cc7a7e801210e2713a251d51a8bf9a77c
TofiOrTofa <mavder76@gmail.com>
2026-04-19 22:40:14 +0500

    feat: добавил парсер и командный центр

--- main.py
+++ main.py
@@ -1,59 +1,9 @@
 #!/usr/bin/env -S python3 -u
 """-onsole colculator"""
 
-from abc import ABC, abstractmethod
-import json
+from parser import *
 import subprocess
 import os
-
-class abstract_calculator(ABC):
-
-    @abstractmethod
-    def compilate(self): ...
-
-    @abstractmethod
-    def score(self): ...
-
-def add_mathematical_sd(math_sing:str, class_math_sing, math_significance:int):
-    #это функция для добавления новых математисестких символов
-    mathematical_sd[math_sing]=(class_math_sing, math_significance)
-
-# классы радители классов математических действий>----------------------------
-
-class compilate_string:
-    """функция для изменения вида переменных"""
-    def compilate_sif(self, str_first, str_second):
-        """меняет вид двух переменных из str в int или float"""
-        if "." in str_first:
-            str_first=float(str_first)
-        else:
-            str_first=int(str_first)
-        if "." in str_second:
-            str_second=float(str_second)
-        else:
-            str_second=int(str_second)
-        #print(str_first, str_second)
-        return str_first, str_second
-        
-
-
-# классы математических действий>---------------------------------------------
-class sum_calculate(abstract_calculator, compilate_string):
-    #класс для сложения чисел
-    def compilate(self, num_first, num_second):
-        return super().compilate_sif(num_first, num_second)
-    def score(self, nums):
-        return str(nums[0]+nums[1])
-class subtraction_calculate(abstract_calculator, compilate_string):
-    #класс для вычитания чисел
-    def compilate(self, num_first, num_second):           
-        return super().compilate_sif(num_first, num_second)
-    def score(self, nums):
-        return str(nums[0]-nums[1])
-
-
-
-#главная часть>-------------------------------------------------------
 
 
 def system():
@@ -64,137 +14,6 @@ def system():
         command=['clear']
     subprocess.run(command, shell=True)
     #>---------------------------------
-
-
-class score_expression:
-    def __init__(self, mathematical_sd):
-        self.numbers : set
-        self.expression : str
-        self.expression_list : list
-        self.SaveExpression : tuple
-        self.TimeExpression_list : list
-        self.TimeExpression_start : int
-        self.TimeExpression_end : int          
-        self.mathematical_sd : dict[str:list(class_sing_calculator, int)]
-        self.TwoExpression : tuple(str, ...)
-        self.TwoExpression_start : int
-        self.backets : bool#наличие скобок в примере
-        self.answer : int or float
-        self.sorterMathSing : list
-
-        self.mathematical_sd = mathematical_sd
-        #mathematical_sings:dict[str:list(str, int)]={}#значимость:[класс, знак]
-        self.numbers={'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-        self.expression=input("Введите выражение: ").strip()
-        self.expression_list=list()
-        self.TimeExpression_list=list()
-        self.main()
-
-    def main(self):
-        self.compilateList() 
-        self.findBacketsExpression()
-        if self.backets: 
-            self.SaveExpression=tuple(self.expression_list)
-            self.compilateBacketsExpression()
-        else:
-            self.backets=False
-            self.TimeExpression_list=self.expression_list
-            self.TimeExpression_start=0
-        while len(self.TimeExpression_list)!=1:
-            self.findTwoExpression()
-            self.scoreTwoExpression()
-            self.removeTwoExpression()
-        print(self.expression_list)
-
-    def sortMathSingByImportance(): ...
-
-
-    def compilateBacketsExpression(self):
-        self.TimeExpression_list=self.expression_list[
-            self.TimeExpression_start+1:self.TimeExpression_end
-        ]
-
-
-    def findBacketsExpression(self):
-        if "(" in self.expression_list:
-            self.TimeExpression_start=self.expression_list.index("(")
-            self.TimeExpression_end=self.expression_list.index(")")
-            self.backets=True
-        else: self.backets=False
-    def findTwoExpression(self):
-        a=1
-        for i in range(len(self.TimeExpression_list)):
-            
-            if self.TimeExpression_list[i] in self.mathematical_sd:
-                testImportanceMathSing=(
-                    self.mathematical_sd[self.TimeExpression_list[i]][1] == a
-                )
-            else: testImportanceMathSing = False
-
-            if testImportanceMathSing:
-                self.TwoExpression:tuple(str, ...)=(
-                    self.TimeExpression_list[i-1:i+2]
-                )
-                self.TwoExpression_start=i-1
-                return 0
-        return 1#вернёт 0 если найдёт выражения со скобками
-
-
-    def scoreTwoExpression(self):
-        self.answer=self.mathematical_sd[self.TwoExpression[1]][0].score(
-            self.mathematical_sd[self.TwoExpression[1]][0].compilate(
-                self.TwoExpression[0], self.TwoExpression[2]
-            )
-        )
-        print(self.answer)
-
-
-    def removeTwoExpression(self):
-        del self.TimeExpression_list[
-            self.TwoExpression_start+1:self.TwoExpression_start+3
-        ]
-        self.TimeExpression_list[self.TwoExpression_start]=self.answer
-        print(self.TimeExpression_list)
-    def removeBacketsExpression(self): ...
-
-    @staticmethod
-    def noneMathSing(breakMathSing): 
-        print("\n\t",
-            "символ", 
-            f"'{breakMathSing}'",
-            "неизвестен",
-            sep=" "
-        )
-        exit()
-
-
-    def compilateList(self):
-        # a==0 - если только что была цыфра, обозначает математический знак
-        # a==1 - если только что был знак, обозначает цифру
-        # a==3 - обозначает скобку, что было до: не важно
-        # a==4 - обозначает плавающую точку у числа, что было до: не важно
-        a=0
-        for i in self.expression:
-            if i in self.numbers:
-                if not a or a==3: self.expression_list.append(i); a=1
-                elif a: self.expression_list[-1]+=i
-            elif i=="(" or i==")": self.expression_list.append(i); a=3
-            elif i=="." or i==",": self.expression_list[-1]+=i; a=4
-            else:
-                if i in self.mathematical_sd:
-                    if not a or a==4: self.expression_list[-1]+=i
-                    else: self.expression_list.append(i); a=0
-                else: self.noneMathSing(i)
-
-
-
-
-
-
-def connection_json():
-    with open('back/history.json', 'r', encoding='utf-8') as f:
-        history = json.load(f)
-
 def manu():
     a=0
     while a>3 or a<=0:
@@ -213,14 +32,22 @@ def manu():
 def main():
     choice=manu()
     if choice == 1:
-        #здесь можно вызывать функцию для добавления новых математических символов
-        add_mathematical_sd("+", sum_calculate(), 1)
-        add_mathematical_sd("-", subtraction_calculate(), 1)
+        math_signs = MathSignsInfo()
+        #вызыов функции для добавления новых математических символов
+        math_signs.add("+", Sum, 1)
+        math_signs.add("-", Difference, 1)
+        math_signs.add("*", Product, 2)
+        math_signs.add("/", Quotient, 2)
+        math_signs.add("%", Ratio, 2)
 
-        score_expression(mathematical_sd)
+        expression_objects = Expression(
+            math_signs,
+            input("Введите выражение: ")
+        )
+        result = expression_objects.score()
+        print("Ответ:", result)
     
 
 if __name__=='__main__':
-    mathematical_sd : dict[str:tuple()]={}
     system()
     main()
--- nodes.py
+++ nodes.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 # абстрактные классы>--------------------
 class Operand(ABC):
     @abstractmethod
-    def __init__(self): ...
+    def __init__(self)->None: ...
     @abstractmethod
     def __add__(self, other): ...
     @abstractmethod
@@ -12,28 +12,23 @@ class Operand(ABC):
     def __mul__(self, other): ...
     @abstractmethod
     def __truediv__(self, other): ...
+class MathSignsAbstract(ABC):
     @abstractmethod
-    def score(self): ...
-class MathSigns(ABC):
+    def __init__ (self, first_operand:object, second_operand:object)->None: ...
     @abstractmethod
-    def __init__ (self,
-        first_operand  : object,
-        second_operand : object
-    ): ...
-    @abstractmethod
-    def score(self): ...
+    def score(self)->object: ...
 
 class MathSignsInfo:
-    def __init__(self):
+    def __init__(self)->None:
         self._signs : dict[
             str : tuple(
                 object,# объект знака
                 int# значимость знака
             )
         ]
-        self._importance : list[
-            int,# минимальное значение
-            int# максимальное значение
+        self._importance : dict[
+            str : str,# минимальное значение
+            str : str# максимальное значение
         ]
         
         self._importance = {
@@ -51,87 +46,119 @@ class MathSignsInfo:
             self._importance["min"] is None or
             self._importance["max"] is None
         ):#>--------------------<#
-            if importance > self._importance["max"]:
-                self._importance["max"] = importance
-            elif importance < self._importance["min"]:
-                self._importance["min"] = importance
+            if importance > self.get_importance(self._importance["max"]):
+                self._importance["max"] = symbol
+            elif importance < self.get_importance(self._importance["min"]):
+                self._importance["min"] = symbol
         else: 
-            self._importance["min"] = importance
-            self._importance["max"] = importance 
+            self._importance["min"] = symbol
+            self._importance["max"] = symbol
 
+            
 
     def get_class(self, symbol:str) -> type:
         return self._signs[symbol][0]
     def get_importance(self, symbol:str) -> int:
         return self._signs[symbol][1]
-    def get_min_importance(self) -> int:
+    def get_min(self) -> str:
         return self._importance["min"]
-    def get_max_importance(self) -> int:
+    def get_max(self) -> str:
         return self._importance["max"]
+    def get_symbol(self, importance:int):
+        key : str; value : tuple[type, int]
+        symbols = list()
+        for key, value in self._signs.items():
+            if importance == value[1]:
+                symbols.append(key)
+        return symbols
     def availability_test(self, symbol:str) -> bool:
         return symbol in self._signs
 
 
-class Constant(Operand):
+class Constant:
     def __init__(self, source:str):
-        self._content = content
+        self._content = source
     def score(self) -> ReducedFraction:
 
         float_content = self._content.replace(",", ".")
-        index = self.float_content.find(".")
+        index = float_content.find(".")
 
         if index != -1:
             quantity = len(self._content) - index
 
             numerator = int(float_content.replace(".", ""))
-            denominator = 10 ** quantity
+            denominator = 10 ** (quantity-1)
             return ReducedFraction(numerator, denominator)
-        return ReducedFraction(int(self._content))
+        return ReducedFraction(int(self._content), 1)
 # матиматические типы данных>--------------------
-class Fraction(Operand):
+class Fraction():
     def __init__(self,
         numerator : Constant,
         denominator : Constant = 1
     ):#>--------------------<#
         self._n : Constant = numerator
         self._d : Constant = denominator
-    def __add__(self, other:object) -> Fraction:# сложение
-        new_n = self._n * other._d + other._n * self._d
-        new_d = self._d * other._d
-        return Fraction(new_n, new_d)
-    def __sub__(self, other:object) -> Fraction:# вычетание
-        new_n = self._n * other._d - other._n * self._d
-        new_d = self._d * other._d
-        return Fraction(new_n, new_d)
-    def __mul__(self, other:object) -> Fraction:# умножение
-        new_n = self._n * other._n
-        new_d = self._d * other._d
-        return Fraction(new_n, new_d)
-    def __truediv__(self, other:object) -> Fraction:# деление
-        new_n = self._n * other._d
-        new_d = self._d * other._n
-        return Fraction(new_n, new_d)
     def score(self): 
         return ReducedFraction(
             self._n.score(),
             self._d.score()
         )
 class ReducedFraction(Operand, Fraction):
-    def __init__(self, n:int, d:int):
+    def __init__(self, n:int, d:int)->None:
         gcp : int = self._find_gcp(n, d)
         self._n : int = n // gcp
-        self._d : int = n // gcp
-    def _find_gcp(self, nr:int, dr:int) -> int, int:
+        self._d : int = d // gcp
+
+    def __str__(self)->str:
+        result:str
+        result:str = f"{self._n}/{self._d}"
+        if self._n % self._d == 0:
+            result = f"{self._n}"
+        elif len(str(self._n % self._d)) < 10:
+            result += f" ({self._n / self._d})"
+        return  result
+        
+    def __add__(self, other:object) -> Fraction:# сложение
+        new_n = self._n * other._d + other._n * self._d
+        new_d = self._d * other._d
+        return ReducedFraction(new_n, new_d)
+    def __sub__(self, other:object) -> Fraction:# вычетание
+        new_n = self._n * other._d - other._n * self._d
+        new_d = self._d * other._d
+        return ReducedFraction(new_n, new_d)
+    def __mul__(self, other:object) -> Fraction:# умножение
+        new_n = self._n * other._n
+        new_d = self._d * other._d
+        return ReducedFraction(new_n, new_d)
+    def __truediv__(self, other:object) -> Fraction:# деление
+        new_n = self._n * other._d
+        new_d = self._d * other._n
+        return ReducedFraction(new_n, new_d)
+
+
+    def _find_gcp(self, nr:int, dr:int) -> int:
         gcd1 : int = nr
         gcd2 : int = dr
         while gcd2 > 0:
             tmp = gcd1 % gcd2
             gcd1, gcd2 = gcd2, tmp
-        return nr
+        return gcd1
 
 
 # объекты мат. знаков>--------------------
-class Sum(MathSigns):
+class Sum(MathSignsAbstract):
+    def __init__(self, first_operand:object, second_operand:object)->None:
+        self._fo : object = first_operand
+        self._so : object = second_operand
+    def score(self):
+        return self._fo.score() + self._so.score()
+class Difference(MathSignsAbstract):
+    def __init__(self, first_operand:object, second_operand:object)->None:
+        self._fo = first_operand
+        self._so = second_operand
+    def score(self)->object:
+        return self._fo.score() - self._so.score()
+class Product(MathSignsAbstract):
     def __init__(self,
         first_operand  : object,
         second_operand : object
@@ -139,41 +166,23 @@ class Sum(MathSigns):
         self._fo : object = first_operand
         self._so : object = second_operand
     def score(self):
-        return self._fo.score() + self._so.score()
-class Difference(MathSigns):
-    def __init__(self,
-        first_operand  : object,
-        second_operand : object
-    ):#>--------------------<#
-        self._fo : object
-        self._so : object
-    def score(self):
-        return self._fo.score() - self._so.score()
-class Product(MathSigns):
-    def __init__(self,
-        first_operand  : object,
-        second_operand : object
-    ):#>--------------------<#
-        self._fo : object
-        self._so : object
-    def score(self):
         return self._fo.score() * self._so.score()
-class Quotient(MathSigns):
+class Quotient(MathSignsAbstract):
     def __init__(self,
         first_operand  : object,
         second_operand : object
     ):#>--------------------<#
-        self._fo : object
-        self._so : object
+        self._fo : object = first_operand
+        self._so : object = second_operand
     def score(self):
         return self._fo.score() / self._so.score()
-class Ratio(MathSigns):
+class Ratio(MathSignsAbstract):
     def __init__(self,
         first_operand  : object,
         second_operand : object
     ):#>--------------------<#
-        self._fo : object
-        self._so : object
+        self._fo : object = first_operand
+        self._so : object = second_operand
     def score(self):
         result = self._fo.score() / self._so.score()
-        return result * ReducedFraction(100)
+        return result * ReducedFraction(100, 1)
